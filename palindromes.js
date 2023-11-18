0568d693eec35a7d8804ed14fd705c85f047efdc
Sasmita Dehury <sasmitadehury.khushboo@gmail.com>
2023-11-18 17:01:24 +0530

    palindrome done

---
+++ palindromes.js
@@ -0,0 +1,21 @@
+function isPalindome(str) {
+  let flipStr = str.split("").reverse().join("");
+
+  if (flipStr === str) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isPalindome("1091"));
+
+//another solution
+function x(str) {
+  let flipStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    flipStr += str[i];
+  }
+  return str === flipStr;
+}
+
+console.log(x("khusi"));
